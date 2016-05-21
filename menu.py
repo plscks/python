cb4918687625d96a66cea9a8df8f9eb16370e324
plscks <plscks@gmail.com>
2016-05-21 04:50:18 +0000

    basic memu

--- menu.py
+++ menu.py
@@ -1,24 +1,41 @@
-import menu
+#!/usr/bin/python
+# Version 1
+## Show menu ##
+import os
+import time
 
-menu = {}
-menu['1'] = 'Add Student.'
-menu['2'] = 'Delete Student.'
-menu['3'] = 'Find Student'
-menu['4'] = 'Exit'
-while True:
-    options = menu.keys()
-    options.sort()
-    for entry in options:
-        print(entry, menu[entry])
 
-    selection = raw_input('Please Select:')
-    if selection == '1':
-        print('add')
-    elif selection == '2':
-        print('delete')
-    elif selection == '3':
-        print('find')
-    elif selection == '4':
-        break
-    else:
-        print('Unknown option selected')
+def new():
+    ROnumber = input('RO Number: ')
+    with open(ROnumber, 'wt') as f:
+        f.write('RO number: ')
+        f.write(ROnumber)
+        f.write('\n')
+    quit()
+
+print (30 * '-')
+print ('   M A I N - M E N U')
+print (30 * '-')
+print ('1. New RO')
+print ('2. Existing RO')
+print ('3. Quit')
+print (30 * '-')
+
+## Get input ###
+choice = input('Enter your choice [1-3] : ')
+
+### Convert string to int type ##
+choice = int(choice)
+
+### Take action as per selected menu-option ###
+if choice == 1:
+    print ('Opening new RO...')
+    time.sleep(2)
+    os.system('clear')
+    new()
+elif choice == 2:
+    print ('Starting user management...')
+elif choice == 3:
+    print ('Rebooting the server...')
+else:  # default ##
+    print ('Invalid number. Try again...')
