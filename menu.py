63e67fdc1f888e01affa936d75dd8cef49b74eb8
plscks <plscks@gmail.com>
2016-05-23 22:03:32 -0500

    Finished menu.py new

--- menu.py
+++ menu.py
@@ -27,33 +27,81 @@ def new():
         for i in range(Lines):
             if i == 0:
                 LineA = input('\nLine A: ')
-               # i = i + 1
                 f.write('Line A: ' + str(LineA))
                 recs()
             elif i == 1:
                 LineB = input('\nLine B: ')
-               # i = i + 1
                 f.write('\n')
                 f.write('\nLine B: ' + str(LineB))
                 recs()
             elif i == 2:
                 LineC = input('\nLine C: ')
-               # i = i + 1
                 f.write('\n')
                 f.write('\nLine C: ' + str(LineC))
                 recs()
-            elif i is Lines:
-                print('\nReturning to menu...')
-                time.sleep(2)
-                menu()
+            elif i == 3:
+                LineD = input('\nLine D: ')
+                f.write('\n')
+                f.write('\nLine D: ' + str(LineD))
+                recs()
+            elif i == 4:
+                LineE = input('\nLine E: ')
+                f.write('\n')
+                f.write('\nLine E: ' + str(LineE))
+                recs()
+            elif i == 5:
+                LineF = input('\nLine F: ')
+                f.write('\n')
+                f.write('\nLine F: ' + str(LineF))
+                recs()
+            elif i == 6:
+                LineG = input('\nLine G: ')
+                f.write('\n')
+                f.write('\nLine G: ' + str(LineG))
+                recs()
+            elif i == 7:
+                LineH = input('\nLine H: ')
+                f.write('\n')
+                f.write('\nLine H: ' + str(LineH))
+                recs()
+            elif i == 8:
+                LineI = input('\nLine I: ')
+                f.write('\n')
+                f.write('\nLine I: ' + str(LineI))
+                recs()
+            elif i == 9:
+                LineJ = input('\nLine J: ')
+                f.write('\n')
+                f.write('\nLine J: ' + str(LineJ))
+                recs()
+            elif i == 10:
+                LineK = input('\nLine K: ')
+                f.write('\n')
+                f.write('\nLine K: ' + str(LineK))
+                recs()
+            elif i == 11:
+                LineL = input('\nLine L: ')
+                f.write('\n')
+                f.write('\nLine L: ' + str(LineL))
+                recs()
+            elif i == 12:
+                LineM = input('\nLine M: ')
+                f.write('\n')
+                f.write('\nLine M: ' + str(LineM))
+                recs()
+        else:
+            print('\nReturning to menu...')
+            time.sleep(2)
+            menu()
 
 
 def menu():
+    os.system('clear')
     print (30 * '-')
     print ('   M A I N - M E N U')
     print (30 * '-')
     print ('1. New RO')
-    print ('2. Existing RO')
+    print ('2. Existing RO - NOT WORKING YET')
     print ('3. Quit')
     print (30 * '-')
     ## Get input ###
@@ -67,11 +115,15 @@ def menu():
         os.system('clear')
         new()
     elif choice == 2:
-        print ('Opening existing RO...')
+        print ('Broken for now...Try again')
+        time.sleep(2)
+        menu()
     elif choice == 3:
         print ('Quitting!')
     else:  # default ##
         print ('Invalid number. Try again...')
+        time.sleep(2)
+        menu()
 
 
 def recs():
@@ -86,4 +138,5 @@ def recs():
 
 os.system('clear')
 menu()
+os.system('clear')
 quit()
