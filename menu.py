f0b96e1ff09deca3edb4b5bdf275ffce686b0d5e
plscks <plscks@gmail.com>
2016-05-26 22:12:28 -0500

    Fixed some errors in main body

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 # Menu.py for tracking Repair orders
 # Written by plscks
+
 import os
 import time
 
@@ -7,85 +8,86 @@ import time
 # ROnumber = 0
 # Lines = 0
 i = 0
+ro = 'f'
 
 
 def new():
     global ROnumber
     global ROfile
-    global f
+    global ro
     ROnumber = input('RO Number: ')
     ROfile = str(ROnumber) + '.txt'
     Lines = input('\nNumber of lines(13 max): ')
     Lines = int(Lines)
-    with open(ROfile, 'wt') as f:
-        f.write('RO number: ')
-        f.write(ROnumber)
-        f.write('\n')
-        f.write('\n')
+    with open(ROfile, 'wt') as ro:
+        ro.write('RO number: ')
+        ro.write(ROnumber)
+        ro.write('\n')
+        ro.write('\n')
         for i in range(Lines):
             if i == 0:
                 LineA = input('\nLine A: ')
-                f.write('Line A: ' + str(LineA))
+                ro.write('Line A: ' + str(LineA))
                 recs()
             elif i == 1:
                 LineB = input('\nLine B: ')
-                f.write('\n')
-                f.write('\nLine B: ' + str(LineB))
+                ro.write('\n')
+                ro.write('\nLine B: ' + str(LineB))
                 recs()
             elif i == 2:
                 LineC = input('\nLine C: ')
-                f.write('\n')
-                f.write('\nLine C: ' + str(LineC))
+                ro.write('\n')
+                ro.write('\nLine C: ' + str(LineC))
                 recs()
             elif i == 3:
                 LineD = input('\nLine D: ')
-                f.write('\n')
-                f.write('\nLine D: ' + str(LineD))
+                ro.write('\n')
+                ro.write('\nLine D: ' + str(LineD))
                 recs()
             elif i == 4:
                 LineE = input('\nLine E: ')
-                f.write('\n')
-                f.write('\nLine E: ' + str(LineE))
+                ro.write('\n')
+                ro.write('\nLine E: ' + str(LineE))
                 recs()
             elif i == 5:
                 LineF = input('\nLine F: ')
-                f.write('\n')
-                f.write('\nLine F: ' + str(LineF))
+                ro.write('\n')
+                ro.write('\nLine F: ' + str(LineF))
                 recs()
             elif i == 6:
                 LineG = input('\nLine G: ')
-                f.write('\n')
-                f.write('\nLine G: ' + str(LineG))
+                ro.write('\n')
+                ro.write('\nLine G: ' + str(LineG))
                 recs()
             elif i == 7:
                 LineH = input('\nLine H: ')
-                f.write('\n')
-                f.write('\nLine H: ' + str(LineH))
+                ro.write('\n')
+                ro.write('\nLine H: ' + str(LineH))
                 recs()
             elif i == 8:
                 LineI = input('\nLine I: ')
-                f.write('\n')
-                f.write('\nLine I: ' + str(LineI))
+                ro.write('\n')
+                ro.write('\nLine I: ' + str(LineI))
                 recs()
             elif i == 9:
                 LineJ = input('\nLine J: ')
-                f.write('\n')
-                f.write('\nLine J: ' + str(LineJ))
+                ro.write('\n')
+                ro.write('\nLine J: ' + str(LineJ))
                 recs()
             elif i == 10:
                 LineK = input('\nLine K: ')
-                f.write('\n')
-                f.write('\nLine K: ' + str(LineK))
+                ro.write('\n')
+                ro.write('\nLine K: ' + str(LineK))
                 recs()
             elif i == 11:
                 LineL = input('\nLine L: ')
-                f.write('\n')
-                f.write('\nLine L: ' + str(LineL))
+                ro.write('\n')
+                ro.write('\nLine L: ' + str(LineL))
                 recs()
             elif i == 12:
                 LineM = input('\nLine M: ')
-                f.write('\n')
-                f.write('\nLine M: ' + str(LineM))
+                ro.write('\n')
+                ro.write('\nLine M: ' + str(LineM))
                 recs()
         else:
             print('\nReturning to menu...')
@@ -94,7 +96,7 @@ def new():
 
 
 def menu():
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print(30 * '-')
     print('   M A I N - M E N U')
     print(30 * '-')
@@ -110,12 +112,12 @@ def menu():
     if choice == 1:
         print('Opening new RO...')
         time.sleep(2)
-        os.system('clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
         new()
     elif choice == 2:
         print('Work in progress...')
         time.sleep(2)
-        os.system('clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
         open()
     elif choice == 3:
         print('Quitting!')
@@ -132,14 +134,14 @@ def recs():
     for j in range(recs):
         j = j + 1
         d[j] = input('Rec #' + str(j) + ': ')
-        f.write('\n    Rec #' + str(j) + ': ' + d[j])
+        ro.write('\n    Rec #' + str(j) + ': ' + d[j])
 
 
-def open():
-
-
-
-os.system('clear')
+# def open():
+#
+#
+#
+os.system('cls' if os.name == 'nt' else 'clear')
 menu()
-os.system('clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 quit()
