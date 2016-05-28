e283042c275c2d5e2be3994cb5f774f0971a04ef
plscks <plscks@gmail.com>
2016-05-28 07:55:14 -0500

    learning file I/O

--- read.py
+++ read.py
@@ -1,8 +1,8 @@
 # test reading file program
 # written by plscks
-import fileinput
+# import fileinput
 import os
-import sys
+# import sys
 
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -11,3 +11,13 @@ print('Open existing RO')
 print(30 * '-')
 ROnumber = input('RO Number: ')
 ROfile = str(ROnumber) + '.txt'
+ROtemp = str(ROnumber) + '_tmp.txt'
+f1 = open(ROfile, 'r')
+f2 = open(ROtemp, 'w')
+for line in f1:
+    f2.write(line.replace('Line A:', '*Line A:'))
+f1.close()
+f2.close()
+os.remove(ROtemp)
+os.rename(ROtemp, ROfile)
+print('edited file?')
