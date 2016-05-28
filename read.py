76be3ad9dde356affcce8230b1f07c871ba82bba
plscks <plscks@gmail.com>
2016-05-28 18:58:58 -0500

    read.py in a functional state

--- read.py
+++ read.py
@@ -10,14 +10,18 @@ print(30 * '-')
 print('Open existing RO')
 print(30 * '-')
 ROnumber = input('RO Number: ')
+line = input('Line: ')
+ROline = 'Line ' + str(line)
+ROdesc = input('New line: ')
+ROnew = '*' + str(ROline) + ': ' + str(ROdesc) + '\n'
 ROfile = str(ROnumber) + '.txt'
 ROtemp = str(ROnumber) + '_tmp.txt'
 f1 = open(ROfile, 'r')
 f2 = open(ROtemp, 'w')
 for line in f1:
-    f2.write(line.replace('Line A:', '*Line A:'))
+    f2.write(ROnew if ROline in line else line)
 f1.close()
 f2.close()
-os.remove(ROtemp)
+os.remove(ROfile)
 os.rename(ROtemp, ROfile)
 print('edited file?')
