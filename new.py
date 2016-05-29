0b7c75e147f21f4ce1df588f0effb5b70261bb81
plscks <plscks@gmail.com>
2016-05-29 11:20:03 -0500

    More changes to menu.py

---
+++ new.py
@@ -0,0 +1,14 @@
+import time
+
+
+def new():
+    print('Here is a function!')
+    print('\nHere is a NEW() function!')
+    print('\nNew line?!')
+
+
+print('going to try to run a function right now')
+time.sleep(2)
+new()
+time.sleep(2)
+print('\nThere we have it, a function!')
--- read.py
+++ read.py
@@ -5,23 +5,46 @@ import os
 # import sys
 
 
-os.system('cls' if os.name == 'nt' else 'clear')
-print(30 * '-')
-print('Open existing RO')
-print(30 * '-')
-ROnumber = input('RO Number: ')
-line = input('Line: ')
-ROline = 'Line ' + str(line)
-ROdesc = input('New line: ')
-ROnew = '*' + str(ROline) + ': ' + str(ROdesc) + '\n'
-ROfile = str(ROnumber) + '.txt'
-ROtemp = str(ROnumber) + '_tmp.txt'
-f1 = open(ROfile, 'r')
-f2 = open(ROtemp, 'w')
-for line in f1:
-    f2.write(ROnew if ROline in line else line)
-f1.close()
-f2.close()
-os.remove(ROfile)
-os.rename(ROtemp, ROfile)
-print('edited file?')
+def newLine():
+    line = input('Line: ')
+    ROline = 'Line ' + str(line)
+    ROdesc = input('New line: ')
+    ROnew = '*' + str(ROline) + ': ' + str(ROdesc) + '\n'
+    f1 = open(ROfile, 'r')
+    f2 = open(ROtemp, 'w')
+    for line in f1:
+        f2.write(ROnew if ROline in line else line)
+    f1.close()
+    f2.close()
+    os.remove(ROfile)
+    os.rename(ROtemp, ROfile)
+
+
+def openMenu():
+    global ROnumber
+    global ROfile
+    global ROtemp
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(30 * '-')
+    print('Open existing RO')
+    print(30 * '-')
+    ROnumber = input('RO Number: ')
+    ROfile = str(ROnumber) + '.txt'
+    ROtemp = str(ROnumber) + '_tmp.txt'
+
+
+def mainMenu():
+    a = input('Edit a line? (y/n): ')
+    if a == 'y':
+        newLine()
+    elif a == 'n':
+        recChange()
+    else:
+        print('Invalid entry! Try again please.')
+        mainMenu()
+
+
+openMenu()
+mainMenu()
+print('changed some stuff maybe?')
+quit()
