ff74d7b692ce71c7ba5e8d4d9c5457b2a250bd33
plscks <plscks@gmail.com>
2016-05-30 03:48:26 -0500

    menu.py nearly working

--- menu.py
+++ menu.py
@@ -96,6 +96,7 @@ def new():
             print('\nReturning to menu...')
             time.sleep(2)
             menu()
+            ro.close()
 
 
 def menu():
@@ -118,10 +119,10 @@ def menu():
         os.system('cls' if os.name == 'nt' else 'clear')
         new()
     elif choice == 2:
-        print('Work in progress...')
+        print('Opening RO...')
         time.sleep(2)
         os.system('cls' if os.name == 'nt' else 'clear')
-        open()
+        openMenu()
     elif choice == 3:
         print('Quitting!')
     else:  # default ##
@@ -140,10 +141,104 @@ def recs():
         ro.write('\n    Rec #' + str(j) + ': ' + d[j])
 
 
-# def open():
-#
-#
-#
+def newLineExit():
+    c = input('Change recommendation? (y/n): ')
+    if c == 'y':
+        recChange()
+    elif c == 'n':
+        menu()  # placeholder for main menu.py
+    else:
+        print('Invalid entry! Try again please :P')
+        newLineExit()
+
+
+def newLine():
+    line = input('Line: ')
+    if line == '':
+        print('Invalid entry! Try again please.')
+        newLine()
+    else:
+        ROline = 'Line ' + str(line)
+        ROdesc = input('New line: ')
+        ROnew = '*' + str(ROline) + ': ' + str(ROdesc) + '\n'
+        f1 = open(ROfile, 'r')
+        f2 = open(ROtemp, 'w')
+        for line in f1:
+            f2.write(ROnew if ROline in line else line)
+        f1.close()
+        f2.close()
+        os.remove(ROfile)
+        os.rename(ROtemp, ROfile)
+        newLineExit()
+
+
+def openMenu():
+    global ROnumber
+    global ROfile
+    global ROtemp
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(30 * '-')
+    print('Open existing RO')
+    print(30 * '-')
+    ROnumber = input('RO Number: ')
+    ROfile = str(ROnumber) + '.txt'
+    ROtemp = str(ROnumber) + '_tmp.txt'
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(30 * '-')
+    print('ORIGINAL RO')
+    print(30 * '-')
+    with open(ROfile, 'rt') as orig:
+        origContent = orig.read()
+        print(origContent)
+    orig.close()
+    mainMenu()
+
+
+def mainMenu():
+    a = input('Change a line? (y/n): ')
+    if a == 'y':
+        newLine()
+    elif a == 'n':
+        b = input('Change a recommendation? (y/n): ')
+        if b == 'y':
+            recChange()
+        elif b == 'n':
+            print('Returning to menu')
+            menu()  # placeholder should go to main menu.py
+        else:
+            print('Invalid entry! Try again please.')
+            mainMenu()
+    else:
+        print('Invalid entry! Try again please.')
+        mainMenu()
+
+
+def recChange():
+    substrLine = input('Which line\'s recommendation to change?: ')
+    substrLine = 'Line ' + substrLine
+    recNum = input('Which rec #?: ')
+    recDesc = input('New recommendation: ')
+    lines = []
+    with open(ROfile, 'rt') as in_file:
+        for line in in_file:
+            lines.append(line)
+    for lineNum, line in enumerate(lines):
+        index = 0
+        st = lines[lineNum]
+        while index < len(st):
+            index = st.find(substrLine, index)
+            if index == -1:
+                break
+            lineNumB = lineNum
+            index += len(substrLine)
+    recNumB = int(lineNumB) + int(recNum)
+    lines[recNumB] = '   *Rec #' + str(recNum) + ': ' + recDesc + '\n'
+    with open(ROfile, 'w') as out_file:
+        out_file.writelines(lines)
+    print('Changed a rec?')
+    in_file.close()
+    out_file.close()
+
 os.system('cls' if os.name == 'nt' else 'clear')
 menu()
 os.system('cls' if os.name == 'nt' else 'clear')
