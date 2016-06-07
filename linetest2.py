f937b227b236d6256bc0330c088b20168096d5f8
plscks <plscks@gmail.com>
2016-06-07 12:46:07 -0500

    optimized new() funtction, will add later

--- linetest2.py
+++ linetest2.py
@@ -2,18 +2,46 @@
 # new line function proof-of-concept
 # written by plscks
 import itertools
+import os
 import string
+import time
 
 
-d = {}
-z = string.ascii_uppercase
-i = 0
-j = []
-lines = input('how many lines: ')
-lines = int(lines)
-for x in range(lines):
-    i = i + 1
-    letter = itertools.cycle(z)
-    j.append(i)
-    d = dict(zip(j, letter))
-    print(d[i])
+def new():
+    global ro
+    d = {}
+    z = string.ascii_uppercase
+    i = 0
+    j = []
+    ROnumber = input('RO Number: ')
+    ROfile = str(ROnumber) + '.txt'
+    Lines = input('Number of lines: ')
+    Lines = int(Lines)
+    with open(ROfile, 'wt') as ro:
+        ro.write('RO number: ')
+        ro.write(ROnumber)
+        ro.write('\n')
+        ro.write('\n')
+        for x in range(Lines):
+            i = i + 1
+            letter = itertools.cycle(z)
+            j.append(i)
+            d = dict(zip(j, letter))
+            newLine = input('Line ' + d[i] + ': ')
+            ro.write('Line ' + d[i] + ': ' + newLine)
+            recs()
+            ro.write('\n')
+
+
+def recs():
+    e = {}
+    recs = input('How many redomendations?: ')
+    recs = int(recs)
+    for k in range(recs):
+        k = k + 1
+        e[k] = input('Rec #' + str(k) + ': ')
+        ro.write('\n    Rec #' + str(k) + ': ' + e[k])
+    ro.write('\n')
+
+
+new()
