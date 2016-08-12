7110c65a6ecb5411a3848819fa35aba6724bb266
plscks <plscks@gmail.com>
2016-08-12 13:34:55 -0500

    Functions fo newOSinit.py

--- newOSinit.py
+++ newOSinit.py
@@ -14,24 +14,59 @@ import shutil
 import urllib.request
 
 
-def main:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    user = input('USERNAME: ')
-    url1 = 'https://raw.githubusercontent.com//plscks/settings/master/.bashrc'
-    path1 = '/home/' + user + '/'
-    file1 = url1[57:]
+os.system('cls' if os.name == 'nt' else 'clear')
+user = input('USERNAME: ')
+url1 = 'https://raw.githubusercontent.com//plscks/settings/master/.bashrc'
+path1 = '/home/' + user + '/'
+file1 = url1[57:]
+url2 = 'https://raw.githubusercontent.com/plscks/settings/master/.emacs'
+path2 = '/home/' + user + '/'
+#url3 = 'https://raw.githubusercontent.com/plscks/settings/master/.bash_aliases'
+#path3 =
+mainIn1
+
+
+def mainIn1:
     if os.path.isdir(path1) == True:
         if os.path.isfile(path1 + file1 == True):
             ow1 = input(file1 + ' exists, overwrite it? (Y/N): ')
             if ow1 == 'Y':
                 print('Overwriting ' + file1)
+                write1
             else:
-                print('Skipping ' + file1
+                print('Skipping ' + file1)
+                mainIn2
         else:
             print('Saving ' + path1 + file1)
+            write1
     else:
         print(path1 + ' Does not exist. Incorrect pathname.')
-    url2 = 'https://raw.githubusercontent.com/plscks/settings/master/.emacs'
-    path2 = '/home/' + user + '/'
-    #url3 = 'https://raw.githubusercontent.com/plscks/settings/master/.bash_aliases'
-    #path3 = 
+        global path1 = input('Input correct path: ')
+        write1
+
+
+def write1:
+    
+
+
+def mainIn2:
+    if os.path.isdir(path2) == True:
+        if os.path.isfile(path2 + file2 == True):
+            ow2 = input(file2 + ' exists, overwrite it? (Y/N): ')
+            if ow2 == 'Y':
+                print('Overwriting ' + file2)
+                write2
+            else:
+                print('Skipping ' + file2)
+                quit()
+        else:
+            print('Saving ' + path2 + file2)
+            write2
+    else:
+        print(path2 + ' Does not exist. Incorrect pathname.')
+        global path2 = input('Input correct path: ')
+        write2
+
+
+def write2:
+    
