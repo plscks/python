f2250121744e3d4f5d4671652c7add1161b2ef8d
plscks <plscks@icloud.com>
2016-11-08 22:33:36 -0600

    Added some debug stuff to dupFinder2

--- dupFinder2.py
+++ dupFinder2.py
@@ -6,16 +6,31 @@
 #
 # written by plscks
 import os
+import time
 
 d = []
 e = []
+f = {}
 
 # d is list of files in cwd
+print('Grabbing a list of files in the CWD')
+time.sleep(2)
 d = os.listdir(os.getcwd())
-# For every file in cwd, size is obtained and added to list e
-for f in d:
-    e.append(os.stat(f).st_size)
+time.sleep(1)
 print(d)
 print()
+# For every file in cwd, size is obtained and added to list e
+print('Creating a list of file sizes from the first list of files')
+time.sleep(2)
+for f in d:
+    e.append(os.stat(f).st_size)
+time.sleep(1)
 print(e)
+print()
 # Now we will zip both lists together to form a dictionary
+print('Zipping both lists together in a dictionary')
+time.sleep(2)
+f = dict(zip(d, e))
+time.sleep(1)
+print(f)
+print()
