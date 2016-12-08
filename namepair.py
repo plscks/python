b9260e6ca148fd0b096a672deee36621327e98f7
Scott Himmelmann <plscks@icloud.com>
2016-12-08 07:51:44 -0600

    added secret santa name pairing program

---
+++ namepair.py
@@ -0,0 +1,18 @@
+# A simple secret santa name pairing script
+
+import os
+
+a = []
+b = []
+c = {}
+
+os.system('cls')
+print('Number of names must be EVEN!')
+
+def fistIn():
+    e = input('Full name : ')
+    a.append(e)
+
+def secondIn():
+    f = input('Full name : ')
+    b.append(f)
