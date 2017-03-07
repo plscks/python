ca5c140bf961939b143bb9689a8a1f2a9d987a56
Scott Himmelmann <plscks@icloud.com>
2017-03-07 03:49:08 +0000

    added sites external address

--- webtest.py
+++ webtest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # testing to see if I can tell if my website is up or down
 # written by plscks
 # References
@@ -6,9 +7,6 @@
 # BCM 17 - blue LED website ALL GOOD
 # BCM 22 - red LED website DOWN
 # BCM 23 - white LED music stream ALL GOOD
-# GPIO.cleanup() is used to reset while in testing phase
-# P.O.C. works as a systemd service
-
 
 import requests
 import RPi.GPIO as GPIO
@@ -19,7 +17,7 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 
-site = 'http://192.168.0.30'
+site = 'http://plscks.ddns.net'
 try:
     status = str(requests.head(site))
 except:
@@ -31,7 +29,7 @@ if status == str('<Response [200]>'):
     print(site + ' is okay right now')
     GPIO.output(17, 1) #Turn on blue LED
     GPIO.output(22, 0) #Make sure red LED is off
-    GPIO.cleanup()
+#    GPIO.cleanup()
 else:
     print(site + ' is busted!! TRIGGER THE ALARMS!')
     print(status)
