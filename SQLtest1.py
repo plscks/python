fe109827f054b3095deab46945ff9fc1dd3e2d90
plscks <plscks@gmail.com>
2017-06-03 14:14:07 -0500

    building interface to SQL database for snort to automate busy work

--- SQLtest1.py
+++ SQLtest1.py
@@ -3,21 +3,42 @@
 # https://www.tutorialspoint.com/python3/python_database_access.htm
 #tinkered with by plscks
 
-import PyMySQL
+import pymysql
+import socket, struct
+
+a = []
 
 # Open database connection
-db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+db = pymysql.connect("localhost","python","python","snort" )
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
+#prepare sql inquiry
+sql = 'select ip_src, count(*) from iphdr group by ip_src;'
+
 # execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+cursor.execute(sql)
 
+results = cursor.fetchall()
+
+#fetch IPs as raw int and load into list
+for row in results:
+    ip_src = row[0]
+    a.append(ip_src)
+
+print(a)
+
+#convert from integers to IPs
+for i in a:
+    ip = socket.inet_ntoa(struct.pack('!L', i))
+    print(ip) 
 # Fetch a single row using fetchone() method.
-data = cursor.fetchone()
+#data = cursor.fetchone()
 
-print ("Database version : %s " % data)
+#print ("Database version : %s " % data)
+
+
 
 # disconnect from server
 db.close()
