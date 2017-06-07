e32fbbfcfa5eebfed96ffcb0540ebc527c82ad23
plscks <plscks@gmail.com>
2017-06-06 21:58:38 -0500

    Sorting IPs by country, need to exclude local IPs from lists

--- SQLtest1.py
+++ SQLtest1.py
@@ -2,11 +2,20 @@
 #Test script lifted from
 # https://www.tutorialspoint.com/python3/python_database_access.htm
 #tinkered with by plscks
+# 'US' 'CA' OK
+#
+# Need to exclude local IPs from lists
+#
+#
+#
 
 import pymysql
 import socket, struct
+from geoip import geolite2
 
-a = []
+raw_ip = []
+good_ip = []
+bad_ip = []
 
 # Open database connection
 db = pymysql.connect("localhost","python","python","snort" )
@@ -25,14 +34,33 @@ results = cursor.fetchall()
 #fetch IPs as raw int and load into list
 for row in results:
     ip_src = row[0]
-    a.append(ip_src)
+    raw_ip.append(ip_src)
 
-print(a)
+print(raw_ip)
 
 #convert from integers to IPs
-for i in a:
+for i in raw_ip:
     ip = socket.inet_ntoa(struct.pack('!L', i))
-    print(ip) 
+    info = geolite2.lookup(ip)
+    print(ip)
+    infotest = info is None
+    if infotest == False:
+        if info.country == 'US' or info.country == 'CA':
+            print(ip + ' Originates from ' + info.country + '. Added to good list')
+            good_ip.append(ip)
+        else:
+            print(ip + ' Originates from ' + info.country + ' I don\'t trust this, added to bad list')
+            bad_ip.append(ip)
+    else:
+        print('No info available, defaulting to bad list')
+        print(ip + ' Added to bad list')
+
+print('----------------GOOD LIST---------------------')
+print(good_ip)
+print()
+print('----------------BAD  LIST---------------------')
+print(bad_ip)
+    
 # Fetch a single row using fetchone() method.
 #data = cursor.fetchone()
 
