93d2bdbd27cc97d5f42b434b7ae88e9d5c52b084
plscks <plscks@icloud.com>
2017-11-17 07:47:19 -0600

    Bash script prevTrack.sh comment

--- prevTrack.sh
+++ prevTrack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# A simple script for poly bar to go to the previous song.
-# written by plscks
+#  A simple script for poly bar to go to the previous song.
+#  written by plscks
 
 if [[ $(mocp -Q %file) = http://192.168.254.31:8001/mpd.mp3 ]]; then
     mpc -q -h 192.168.254.31 prev
