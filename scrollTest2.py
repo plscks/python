938c374fc47177b597c006d74c115e166f501ca1
Scott Himmelmann <plscks@icloud.com>
2018-03-15 07:52:25 -0500

    Damn damn damn...?

--- scrollTest2.py
+++ scrollTest2.py
@@ -4,5 +4,10 @@
 #
 # Is this a test?
 # It has to be.....
+#
+# example from:
+# https://codegolf.stackexchange.com/questions/58732/make-me-a-scrolling-marquee
 
 
+import time;s=input()*80
+while 1:print("\033[2J",end=s[:80],flush=1);s=s[1:]+s[0];time.sleep(.1)
