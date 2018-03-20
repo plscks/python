da258f50a20aa2e28f90c8d045cf02b4efae94ba
Scott Himmelmann <plscks@icloud.com>
2018-03-19 21:26:00 -0500

    Oh gosh, I didnt add a file at work today, crap-it-all

---
+++ scrollTest4.py
@@ -0,0 +1,16 @@
+ # Yet another scroll test, or try, or whatever
+ # it is that I'm doing here?
+ # Messing list lists and itterating??
+ # Written by plscks
+ #
+ # Is this a test?
+ # It has, yeah I guess, whatever.....
+
+import time
+
+a = []
+string = input('Have you itered much today? String please: ')
+for letter in string:
+    a.append(letter)
+    print(a)
+    time.sleep(2)
