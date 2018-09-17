2f941269219160e4b1b71dd889768d8d79c0589c
Scott Himmelmann <plscks@icloud.com>
2018-09-16 22:03:57 -0500

    update

--- discordBotTest1.py
+++ discordBotTest1.py
@@ -134,7 +134,7 @@ async def multiply(ctx, a: int, b: int):
 
 @bot.command()
 async def greet(ctx):
-    await ctx.send(":kitty: Why hello, there!")
+    await ctx.send(":eggplant: Why hello, there! :kitty:491077122434138124")
 
 @bot.command()
 async def fORB(ctx):
