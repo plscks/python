a2b3fea6c79de7d34aa7388e38c855b87015cb03
plscks <plscks@gmail.com>
2019-06-23 10:05:03 -0500

    Adds final commit to gridTest, moving over to working repository

--- gridTest.py
+++ gridTest.py
@@ -1,55 +1,70 @@
+# NC Map Maker - Alpha 0.5
 # This is a test program, I am trying to see if I can adapt it to my
 # Nexus Clash mapping project, anyways I lifted it from here:
 # https://stackoverflow.com/questions/19586828/drawing-grid-pattern-in-matplotlib
+# I have borrowed the coordinate encode/decode system from the NC hypermap
+# which I happen to be the curator of currently
 #
 # Onward to the show!
+#
+#
+#############
+##  GOALS  ##
+#############
+# [] - Load csv file and aqcuire data
+# [] - Assemble data array where physical location stores the encoded coordinate data
+# [] - Store hex color in that space of the color map
+# [] - print map to file
+import math
 import matplotlib.colors
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 15
+def encodeLocation(x, y, plane):
+    """Takes a tuple of coordinates (x, y, z) and returns a unique identifier"""
+    val = x + y*50 + plane*2500
+    return val
+
+def decodeLocation(val):
+    """Returns a tuple that the initial coordinate was (x, y, z)"""
+    result = [None] * len(2)
+    result[2] = math.floor(val/2500)
+    result[1] = math.floor((val - result[2]*2500)/50)
+    result[0] = math.floor((val - result[1]*50)%50)
+    return result
+
+
+# map size (N x N)
+N = 70
+
+# main list of decoded coordinates
+# the encoded location will store the color
+# ie - location (1,2,0) has a color of #FF00BB
+# main[101] = '#FF00BB'
+main = [None] * 12000
+
 # make an empty data set
-#data = np.ones((N, N)) * np.nan
-
-data = np.chararray((N, N), itemsize=7)
-data[:] = 'nan'
-print(data)
-data[0][1] = '#FF0033'
-data[0][2] = '#BB22FF'
-data[0][3] = '#00FF22'
-data[1][4] = '#B4B4FF'
-
-# fill in some fake data
-
-# for j in range(3)[::-1]:
-#     data[N//2 - j : N//2 + j +1, N//2 - j : N//2 + j +1] = j
+data = np.ones((N, N)) * np.nan
 
 # make a figure + axes
-
-print('Whole data set:')
-print(data)
-print('This is how we select an individual position: data[0][2]')
-print(data[0][2])
-print('Here is another one: data[1][4]')
-print(data[1][4])
-
-# So what needs to happen is the data set needs to represent
-# the coordinate system and the hex color code will be the data
-# within. Then we need to figure out how it takes that map and
-# applies color to the picture.
-
 fig, ax = plt.subplots(1, 1, tight_layout=True)
+
 # make color map
 my_cmap = matplotlib.colors.ListedColormap(['r', 'g', 'b'])
-# set the 'bad' values (nan) to be white and transparent
-my_cmap.set_bad(color='k', alpha=1)
-# draw the grid
+
+# set the 'bad' values (nan) to be black
+my_cmap.set_bad(color='k')
+
+# draw the grid as black lines:'k' or white lines:'w'
 for x in range(N + 1):
-    ax.axhline(x, lw=2, color='w', zorder=5)
-    ax.axvline(x, lw=2, color='w', zorder=5)
+    ax.axhline(x, lw=2, color='k', zorder=5)
+    ax.axvline(x, lw=2, color='k', zorder=5)
+
 # draw the boxes
 ax.imshow(data, interpolation='none', cmap=my_cmap, extent=[0, N, 0, N], zorder=0)
 
 # turn off the axis labels
 ax.axis('off')
+
+# Output png
 plt.savefig('books_read.png')
