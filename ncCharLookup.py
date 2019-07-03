c6f7de0bc746c52e9158f1a371b745efcc0b2f42
plscks <plscks@gmail.com>
2019-07-02 20:45:45 -0500

    Added Nexus Clash Char Lookup utility

---
+++ ncCharLookup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Nexus Clash character profile info lookup by name
+# Written by plscks
+#
+# Let's see if I can get this thing to work well
+# Let's see if I can get this thing to work at all?!
+# Is this a test, it has to be.....
+#
+#################
+## TO DO LIST  ##
+#################
+# [] Use Kivy to make a GUI that is cross compatable
+# [] Hook into NC charater profile API
+# [] Have a text field to type a name
+# [] Lookup data and get json
+# [] Parse json into readable format and display in copy/pastable format
+# [] Actually show faction name
+# [] Actually show avatar
+# [] List how many exploration badges they have and how many remain to be gotten
+# [] List which stat grinds they have completed
+# [] write program like a real program and not in dumb testing segments
+# [] If need to do testing, clear it out and refactor
+#
+####################
+##  HELPFUL LINKS ##
+####################
+# https://www.nexusclash.com
+# https://www.nexusclash.com/modules.php?name=Character&charname=[NAME GOES HERE]&format=json
+# ----> Uses '%20' as a space
+# Following this kivy guide: https://likegeeks.com/kivy-tutorial/
+#
+# ONWARDS!!
+from kivy.app import App
