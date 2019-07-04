ba9f0fb04625cda0bd3c85eb762a38a043cc2e94
plscks <plscks@gmail.com>
2019-07-04 12:12:52 -0500

    Removed unused counting function

--- ncCharLookup.py
+++ ncCharLookup.py
@@ -32,18 +32,11 @@
 # ONWARDS!!
 import tkinter as tk
 
-counter = 0
-def counter_label(label):
-  def count():
-    global counter
-    counter += 1
-    label.config(text=str(counter))
-    label.after(1000, count)
-  count()
 
 def testing(text):
     print('Button clicked!')
     print(text)
+
 
 root = tk.Tk()
 root.title('Counting Seconds')
