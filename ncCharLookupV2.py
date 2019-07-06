50eadf68bcce3169460b1382d8ddd9b9c200f65a
plscks <plscks@icloud.com>
2019-07-06 16:20:59 -0500

    Updated

--- ncCharLookupV2.py
+++ ncCharLookupV2.py
@@ -51,58 +51,67 @@ class MainApplication(tk.Frame):
         queryNameLabel.grid(row=2, column=0)
         queryEntryField = tk.Entry(self, textvariable=charName)
         queryEntryField.grid(row=2, column=1)
-        lookupBtn = tk.Button(self, text='Lookup', width=6, command=lambda: self.infoGather(charName.get()))
-        lookupBtn.grid(row=2, column=2)
+        adStatusField = tk.Label(self, text='', width=40)
+        adStatusField.grid(row=3, column=1, columnspan=1)
+        lookupBtn = tk.Button(self, text='Lookup', width=6, command=lambda: self.infoGather(charName.get(), adStatusField))
+        lookupBtn.grid(row=2, column=2, pady=10)
         exit = tk.Button(self, text='QUIT', width=20, command=root.destroy)
-        exit.grid(row=18, column=2)
-        gridInit()
+        exit.grid(row=18, column=2, padx=10, pady=10)
+        self.gridInit()
 
 
     def gridInit(self):
         # Create buttons
         avatarField = tk.Canvas(self, width = 250, height = 250)
         charNameLabel = tk.Label(self, text='Character Name: ')
-        charNamePanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black')
+        charNamePanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black', width=25)
         charIDLabel = tk.Label(self, text='Character ID: ')
-        charIDPanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black')
+        charIDPanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black', width=25)
         profileBtn = tk.Label(self, text="profile", fg="blue", cursor="hand2")
+        charClassInfoLabel = tk.Label(self, text='Character Level & Class')
+        charClassInfoPanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black', width=25)
 
-        # Position buttons
-        avatarField.grid(row=3, rowspan=15, column=0)
-        charNameLabel.grid(row=3, column=1, sticky=tk.E)
-        charNamePanel.grid(row=3, column=2, columnspan=2)
-        charIDLabel.grid(row=4, column=1, sticky=tk.E)
-        charIDPanel.grid(row=4, column=2, columnspan=2)
-        profileBtn.grid(row=5, column=2)
 
-    def infoGather(self, inName):
+        # Position of buttons
+        avatarField.grid(row=4, rowspan=15, column=0)
+        charNameLabel.grid(row=4, column=1, sticky=tk.E)
+        charNamePanel.grid(row=4, column=2, columnspan=2)
+        charIDLabel.grid(row=5, column=1, sticky=tk.E)
+        charIDPanel.grid(row=5, column=2, columnspan=2)
+        charClassInfoLabel.grid(row=6, column=1, sticky=tk.E)
+        charClassInfoPanel.grid(row=6, column=2, columnspan=3)
+        profileBtn.grid(row=3, column=2)
+
+
+    def infoGather(self, inName, status):
         inName.replace(' ', '%20')
         response = requests.get('https://www.nexusclash.com/modules.php?name=Character&charname=' + str(inName) + '&format=json')
         data = response.json()
 
-        tk.Label(self, text='Character Name: ').grid(row=3, column=1, sticky=tk.E)
+        tk.Label(self, text='Character Name: ').grid(row=4, column=1, sticky=tk.E)
         collectedName = tk.StringVar()
         collectedName.set(data['result']['character']['name']['name'])
-        charNamePanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black')
+        charNamePanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black', width=25)
         charNamePanel.config(textvariable=collectedName, relief='flat')
-        charNamePanel.grid(row=3, column=2, columnspan=2)
+        charNamePanel.grid(row=4, column=2, columnspan=2)
 
-        tk.Label(self, text='Character ID: ').grid(row=4, column=1, sticky=tk.E)
+        tk.Label(self, text='Character ID: ').grid(row=5, column=1, sticky=tk.E)
         collectedID = tk.StringVar()
         collectedID.set(data['result']['character']['id'])
-        charIDPanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black')
+        charIDPanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black', width=25)
         charIDPanel.config(textvariable=collectedID, relief='flat')
-        charIDPanel.grid(row=4, column=2, columnspan=2)
+        charIDPanel.grid(row=5, column=2, columnspan=2)
 
         profileBtn = tk.Label(self, text="profile", fg="blue", cursor="hand2")
         profileBtn.bind('<Button-1>', lambda e: self.callback('https://www.nexusclash.com/modules.php?name=Game&op=character&id=' + str(data['result']['character']['id'])))
-        profileBtn.grid(row=5, column=2)
+        profileBtn.grid(row=3, column=2)
 
         try:
             URL = data['result']['character']['avatar']['url'].replace(' ', '%20')
             u = urlopen(URL)
-        except urllib.error.HTTPError:
-            u = urlopen()
+        except:
+            URL = 'https://github.com/plscks/python/raw/master/nochar.png'
+            u = urlopen(URL)
         raw_data = u.read()
         u.close()
         im = Image.open(BytesIO(raw_data))
@@ -115,6 +124,24 @@ class MainApplication(tk.Frame):
         avatar.image = photo
         avatar.create_image(5, 127, anchor=tk.W, image=photo)
         avatar.grid(row=3, rowspan=15, column=0)
+
+        if data['result']['character']['status']['alive'] == True:
+            status.config(text='This character is currently alive.')
+        else:
+            status.config(text='This character is floating above the planes.')
+        if data['result']['character']['id'] == 0:
+            status.config(text='No character exists with this name.')
+
+        charClass = charClass = len(data['result']['character']['classes']) - 1
+        collectedLevel = str(data['result']['character']['level'])
+        collectedClass = data['result']['character']['classes'][charClass]
+        collectedClassInfo = tk.StringVar()
+        collectedClassInfo.set('Level ' + collectedLevel + ' ' + collectedClass)
+        charClassInfoLabel = tk.Label(self, text='Character Level & Class')
+        charClassInfoPanel = tk.Entry(self, state='readonly', readonlybackground='white', fg='black', width=25)
+        charClassInfoLabel.grid(row=6, column=1, sticky=tk.E)
+        charClassInfoPanel.config(textvariable=collectedClassInfo, relief='flat')
+        charClassInfoPanel.grid(row=6, column=2, columnspan=3)
 
 
     def callback(self, url):
