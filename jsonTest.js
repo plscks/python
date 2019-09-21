5ef65e7ab249ed4712bf96a711ab4c6890f2b186
plscks <plscks@icloud.com>
2019-09-21 08:57:32 -0500

    js json file test

---
+++ jsonTest.js
@@ -0,0 +1,5 @@
+// Let's attempt to play around with json data imported in from .json file
+// filename in question will be searchRates.json
+// written by plscks
+// Is this a test?
+// It has to be.....
