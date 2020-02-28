d5f0211c73b48a8e63d6c8d8b68a5c9e283becb0
plsck <plscks@icloud.com>
2020-02-27 19:35:30 -0600

    This has to be wrong

--- sysinforTest.py
+++ sysinforTest.py
@@ -4,7 +4,7 @@ from imgrender import render
 
 def render(path, scale=(60, 60)):
     renderer = imgrender.Renderer()
-    image = get_image(path)
+    image = imgrender.get_image(path)
     output = renderer.render_image(image, scale)
     print('\n'.join([''.join(row) for row in output]))
 
