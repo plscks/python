755284335ad81d51fca11e7978ada4967fed739a
plsck <plscks@icloud.com>
2020-02-27 19:15:37 -0600

    Further testing

--- sysinforTest.py
+++ sysinforTest.py
@@ -7,6 +7,8 @@ class Renderer():
         renderer = Renderer()
         image = get_image(path)
         output = renderer.render_image(image, scale)
-        print('\n'.join(['||'.join(row) for row in output]))
+        for row in output:
+            print(row)
+        print('\n'.join([''.join(row) for row in output]))
 
 render("/home/plscks/artwork-the-starry-night.jpg", (20, 20))
