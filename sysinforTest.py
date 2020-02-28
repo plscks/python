7098418a932db5fa92b85c8da9bc7d38e45da340
plsck <plscks@icloud.com>
2020-02-27 19:10:34 -0600

    Might work this time

--- sysinforTest.py
+++ sysinforTest.py
@@ -1,10 +1,12 @@
 #!/bin/python3
 from imgrender import render
 
-def render(path, scale=(60, 60)):
-    renderer = Renderer()
-    image = get_image(path)
-    output = renderer.render_image(image, scale)
-    print('\n'.join([''.join(row) for row in output]))
+class Renderer():
+
+    def render(path, scale=(60, 60)):
+        renderer = Renderer()
+        image = get_image(path)
+        output = renderer.render_image(image, scale)
+        print('\n'.join([''.join(row) for row in output]))
 
 render("/home/plscks/artwork-the-starry-night.jpg", (20, 20))
