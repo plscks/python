213e854ceca21e25a1f524f8d26e0a05fdb576cb
plsck <plscks@icloud.com>
2020-02-27 20:37:08 -0600

    Colored text

--- sysinforTest.py
+++ sysinforTest.py
@@ -7,13 +7,17 @@ def getInfo():
     info = platform.uname()
     return info
 
+def colorString(color, text):
+    colored_text = f"\033[{color}{text}\033[00m"
+    return colored_text
+
 def render(path, scale=(60, 60)):
+    red = '34m'
     renderer = imgrender.Renderer()
     image = imgrender.get_image(path)
     output = renderer.render_image(image, scale)
-    print(output[1])
     extraInfo = getInfo()
-    output[1].append(f'    SYSTEM: {extraInfo[0]}')
+    output[1].append(colorString(red, '            SYSTEM: ') +  f'{extraInfo[0]}')
     print('\n'.join([''.join(row) for row in output]))
 
 render("/home/plscks/artwork-the-starry-night.jpg", (20, 20))
