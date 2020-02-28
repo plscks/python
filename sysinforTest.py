283dac06bea0379176eb14d66d03191c47ff91a8
plsck <plscks@icloud.com>
2020-02-27 19:34:11 -0600

    Oh no!

--- sysinforTest.py
+++ sysinforTest.py
@@ -1,7 +1,11 @@
 #!/bin/python3
+import imgrender
 from imgrender import render
 
 def render(path, scale=(60, 60)):
-    print("THIS RAN")
+    renderer = Renderer()
+    image = get_image(path)
+    output = renderer.render_image(image, scale)
+    print('\n'.join([''.join(row) for row in output]))
 
 render("/home/plscks/artwork-the-starry-night.jpg", (20, 20))
