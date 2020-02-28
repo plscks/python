9e3b977ace20edb2cbf724281b1bda3678bf00a6
plsck <plscks@icloud.com>
2020-02-27 19:54:44 -0600

    Modify

--- sysinforTest.py
+++ sysinforTest.py
@@ -1,11 +1,18 @@
 #!/bin/python3
 import imgrender
 from imgrender import render
+import platform
+
+def getInfo():
+    info = platform.uname()
+    return info
 
 def render(path, scale=(60, 60)):
     renderer = imgrender.Renderer()
     image = imgrender.get_image(path)
     output = renderer.render_image(image, scale)
-    print('||\n'.join([''.join(row) for row in output]))
+    for row in output:
+        print(row)
+    #print('\n'.join([''.join(row) for row in output]))
 
 render("/home/plscks/artwork-the-starry-night.jpg", (20, 20))
