952cae0224e68346df8237fb68370a5e9bba2d21
plsck <plscks@icloud.com>
2020-03-12 22:28:56 -0500

    Accidentally went emacs mode with C-X C-S and erased the line

--- i2ctest.py
+++ i2ctest.py
@@ -50,6 +50,7 @@ x = 0
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype('/home/plscks/FiraCode-Regular.ttf', 9)
 
 while True:
 
