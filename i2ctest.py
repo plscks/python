e959079cf2e69a965c2ae30c10d0c67aa0d4908f
plsck <plscks@icloud.com>
2020-03-12 22:37:57 -0500

    Testing output

--- i2ctest.py
+++ i2ctest.py
@@ -60,12 +60,16 @@ draw.rectangle((0, 0, width, height), outline=0, fill=0)
 # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
 cmd = "hostname -I | cut -d\' \' -f1"
 IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+print('IP')
 cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
 CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+print(CPU)
 cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
 MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+print(MemUsage)
 cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}'"
 Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+print(Disk)
 
 # Write four lines of text.
 
