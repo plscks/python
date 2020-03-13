6c56eafe865abf6064c6661134b02f28fed3fb38
plsck <plscks@icloud.com>
2020-03-12 22:44:06 -0500

    How many lines can this thing deal with

--- i2ctest.py
+++ i2ctest.py
@@ -52,34 +52,19 @@ x = 0
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 font = ImageFont.truetype('/home/plscks/FiraCode-Regular.ttf', 9)
 
-
 # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-# Shell scripts for system monitoring from here:
-# https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-cmd = "hostname -I | cut -d\' \' -f1"
-IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-print('IP')
-cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-print(CPU)
-cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-print(MemUsage)
-cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}'"
-Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-print(Disk)
 
 # Write four lines of text.
 
-draw.text((x, top+0), "IP: "+IP, font=font, fill=255)
-draw.text((x, top+8), CPU, font=font, fill=255)
-draw.text((x, top+16), MemUsage, font=font, fill=255)
-draw.text((x, top+24), Disk, font=font, fill=255)
-
-# Test sentance
-draw.text((x, top+32), '~TEST~test~TEST~', font = font, fill = 255)
+draw.text((x, top+0), 'Line 1', font=font, fill=255)
+draw.text((x, top+8), 'Line 2', font=font, fill=255)
+draw.text((x, top+16), 'Line 3', font=font, fill=255)
+draw.text((x, top+24), 'Line 4', font=font, fill=255)
+draw.text((x, top+32), 'Line 5', font = font, fill = 255)
+draw.text((x, top+40), 'Line 6', font = font, fill = 255)
+draw.text((x, top+48), 'Line 7', font = font, fill = 255)
 
 # Display image.
 disp.image(image)
